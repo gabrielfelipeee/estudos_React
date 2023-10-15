7d5cea888661a4608b3953277f3a0874689240df
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-10-15 00:04:45 -0300

    Criando o banner

---
+++ 1_organo_react-com-js/src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import styles from "./styles.css";
+
+export default function Banner() {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="Banner principal da página do Organo"></img>
+        </header>
+    )
+};
