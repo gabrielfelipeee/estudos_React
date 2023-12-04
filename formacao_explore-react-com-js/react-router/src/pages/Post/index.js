7210719c625954a07d8eb38a1d3787d679d35696
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-12-04 08:56:09 -0300

    Usando a biblioteca ReactMarkdown

--- formacao_explore-react-com-js/react-router/src/pages/Post/index.js
+++ formacao_explore-react-com-js/react-router/src/pages/Post/index.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 import './styles.scss';
 
 import posts from '../../mocks/posts.json';
@@ -11,12 +12,19 @@ const Post = () => {
     const params = useParams();
     const post = posts.find(post => post.id === Number(params.id));
 
+    if (!post) {
+        return <h1>Page not fouder</h1>
+    }
+
     return (
         <PostTemplate
             cover={`/assets/posts/${params.id}/cover.png`}
             title={post.title}>
 
             <div className='box-post'>
+                <ReactMarkdown>
+                    {post.text}
+                </ReactMarkdown>
             </div>
         </PostTemplate>
     )
