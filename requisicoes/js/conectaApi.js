13aca311fef515159cb826aa85e65f8808117c39
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2024-02-18 19:23:31 -0300

    Criando uma requisição POST

--- requisicoes/js/conectaApi.js
+++ requisicoes/js/conectaApi.js
@@ -1,12 +1,11 @@
-const lista = document.querySelector("[data-lista]");
-
+// Requisição GET
 async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos");
     const conexaoConvertida = await conexao.json();
     return conexaoConvertida;
 }
 
-
+const lista = document.querySelector("[data-lista]");
 function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
     video.className = "videos__item";
@@ -14,12 +13,13 @@ function constroiCard(titulo, descricao, url, imagem) {
     <iframe width="100%" height="72%" src="${url}"
     title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen></iframe>
-<div class="descricao-video">
-    <img src="${imagem}" alt="logo canal alura">
-    <h3>${titulo}</h3>
-    <p>${descricao}</p>
-</div>`
+    allowfullscreen>
+    </iframe>
+    <div class="descricao-video">
+        <img src="${imagem}" alt="logo canal alura">
+        <h3>${titulo}</h3>
+        <p>${descricao}</p>
+    </div>`
     return video;
 };
 
@@ -29,3 +29,40 @@ async function listaVideo() {
         constroiCard(elemento.titulo, elemento.descricao, elemento.imagem, elemento.url)))
 }
 listaVideo();
+
+
+
+
+// Requisição POST
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json" // tipo de arquivo
+        },
+        body: JSON.stringify({ // dados
+            titulo: titulo,
+            descricao: `${descricao} visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+};
+
+
+const formulario = document.querySelector("[data-formulario]");
+async function criarVideo(event) {
+    event.preventDefault();
+
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random * 10).toString();
+
+    await criaVideo(titulo, descricao, url, imagem);
+    window.location.href = "../pages/envio-concluido.html";
+}
+
+formulario.addEventListener('submit', (event) => criaVideo(event));
